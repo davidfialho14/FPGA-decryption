6d6337fd9808fcb2b8e9b4c2c5994e44deb0854a
davidfialho <fialho.david@gmail.com>
2015-03-21 23:45:25 +0000

    bloco passa a ser inicializado com o bloco do exemplo

--- src/block.c
+++ src/block.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 #include "block.h"
+
+const uint8_t defaultBlock[] =
+{
+  0x2b, 0x28, 0xab, 0x09,
+  0x7e, 0xae, 0xf7, 0xcf,
+  0x15, 0xd2, 0x15, 0x4f,
+  0x16, 0xa6, 0x88, 0x3c
+};
 
 void printBlock(Block block) {
   uint8_t i, j;
@@ -14,10 +23,5 @@ void printBlock(Block block) {
 }
 
 void initBlock(Block block) {
-  uint8_t i, j;
-  for(i = 0; i < BLOCKLENGTH; ++i) {
-    for(j = 0; j < BLOCKLENGTH; ++j) {
-      block[BPOS(i, j)] = (uint8_t) (rand() % 256);
-    }
-  }
+  memcpy(block, defaultBlock, BLOCKSIZE);
 }
