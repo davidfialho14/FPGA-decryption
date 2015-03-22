79ca3aaea704d9c0b8f67b5dd32364cbc5cfc459
davidfialho <fialho.david@gmail.com>
2015-03-22 12:01:16 +0000

    implementacao da inversa do mix columns

--- src/decryption.c
+++ src/decryption.c
@@ -2,6 +2,12 @@
 #include "block.h"
 #include "rcon.h"
 
+// xtime is a macro that finds the product of {02} and the argument to xtime modulo {1b}
+#define xtime(x)   ((x<<1) ^ (((x>>7) & 1) * 0x1b))
+
+// Multiplty is a macro used to multiply numbers in the field GF(2^8)
+#define Multiply(x,y) (((y & 1) * x) ^ ((y>>1 & 1) * xtime(x)) ^ ((y>>2 & 1) * xtime(xtime(x))) ^ ((y>>3 & 1) * xtime(xtime(xtime(x)))) ^ ((y>>4 & 1) * xtime(xtime(xtime(xtime(x))))))
+
 extern const uint8_t rcon[RCONSIZE];
 
 const uint8_t invSbox[256] =
@@ -43,5 +49,29 @@ void invSubBytes(Block a) {
     for(j = 0; j < BLOCKLENGTH; ++j) {
       a[BPOS(i, j)] = invSbox[a[BPOS(i, j)]];
     }
+  }
+}
+
+void addRoundKey(Block a, Block key) {
+  uint8_t i, j;
+  for(i = 0; i < BLOCKLENGTH; ++i) {
+    for(j = 0; j < BLOCKLENGTH; ++j) {
+      a[BPOS(i, j)] ^= key[(BPOS(i, j))];
+    }
+  }
+}
+
+void invMixColumns(Block a) {
+  uint8_t aux[BLOCKLENGTH];
+  uint8_t i, j;
+  for(j = 0; j < BLOCKLENGTH; ++j) {
+    for(i = 0; i < BLOCKLENGTH; ++i) {
+      aux[i] = a[BPOS(i, j)];
+    }
+
+    a[BPOS(0, j)] = Multiply(aux[0], 0x0e) ^ Multiply(aux[1], 0x0b) ^ Multiply(aux[2], 0x0d) ^ Multiply(aux[3], 0x09);
+    a[BPOS(1, j)] = Multiply(aux[0], 0x09) ^ Multiply(aux[1], 0x0e) ^ Multiply(aux[2], 0x0b) ^ Multiply(aux[3], 0x0d);
+    a[BPOS(2, j)] = Multiply(aux[0], 0x0d) ^ Multiply(aux[1], 0x09) ^ Multiply(aux[2], 0x0e) ^ Multiply(aux[3], 0x0b);
+    a[BPOS(3, j)] = Multiply(aux[0], 0x0b) ^ Multiply(aux[1], 0x0d) ^ Multiply(aux[2], 0x09) ^ Multiply(aux[3], 0x0e);
   }
 }
