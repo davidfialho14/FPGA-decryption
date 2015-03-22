0ecc00cd6a2d7373fe4960bd36f7b95e10ec0712
davidfialho <fialho.david@gmail.com>
2015-03-22 00:30:36 +0000

    passam a fazer-se 10 rondas de forma a implementar o key scheduling completo

--- src/main.c
+++ src/main.c
@@ -71,35 +71,38 @@ void shiftRows(Block a) {
 }
 
 void keySchedule(Block key) {
-  uint8_t i;
+  uint8_t i, n;
   uint8_t col[BLOCKLENGTH]; // coluna auxiliar
-  // rodar coluna
-  for(i = 0; i < BLOCKLENGTH - 1; ++i) {
-    col[i] = key[BPOS(i + 1, 3)];
-  }
-  col[3] = key[BPOS(0, 3)];
 
-  // aplicar subbyte a cada byte da coluna
-  for(i = 0; i < BLOCKLENGTH; ++i) {
-    col[i] = sbox[col[i]];
-  }
+  for(n = 0; n < 10; ++n) { // fazer 10 rondas
+    // rodar coluna
+    for(i = 0; i < BLOCKLENGTH - 1; ++i) {
+      col[i] = key[BPOS(i + 1, 3)];
+    }
+    col[3] = key[BPOS(0, 3)];
 
-  // fazer XOR entre a ultima coluna e a primeira
-  for(i = 0; i < BLOCKLENGTH; ++i) {
-    col[i] = key[BPOS(i, 0)] ^ col[i];
-  }
-
-  // fazer xor entre o resultado anterior e a coluna de rcon e guardar resultado na primeira coluna
-  key[BPOS(0, 0)] = col[0] ^ rcon[1];
-  for(i = 1; i < BLOCKLENGTH; ++i) {
-    key[BPOS(i, 0)] = col[i] ^ 0x00;
-  }
-
-  // fazer round das proximas 3 colunas
-  uint8_t j;
-  for(j = 1; j < BLOCKLENGTH; ++j) {
+    // aplicar subbyte a cada byte da coluna
     for(i = 0; i < BLOCKLENGTH; ++i) {
-      key[BPOS(i, j)] ^= key[BPOS(i, j - 1)];
+      col[i] = sbox[col[i]];
+    }
+
+    // fazer XOR entre a ultima coluna e a primeira
+    for(i = 0; i < BLOCKLENGTH; ++i) {
+      col[i] = key[BPOS(i, 0)] ^ col[i];
+    }
+
+    // fazer xor entre o resultado anterior e a coluna de rcon e guardar resultado na primeira coluna
+    key[BPOS(0, 0)] = col[0] ^ rcon[n+1];
+    for(i = 1; i < BLOCKLENGTH; ++i) {
+      key[BPOS(i, 0)] = col[i] ^ 0x00;
+    }
+
+    // fazer round das proximas 3 colunas
+    uint8_t j;
+    for(j = 1; j < BLOCKLENGTH; ++j) {
+      for(i = 0; i < BLOCKLENGTH; ++i) {
+        key[BPOS(i, j)] ^= key[BPOS(i, j - 1)];
+      }
     }
   }
 }
