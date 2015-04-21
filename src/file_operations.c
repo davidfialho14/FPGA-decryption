0f83779847fd3141a12bc2199f1b6d8235d6628b
davidfialho <fialho.david@gmail.com>
2015-04-21 23:01:19 +0100

    corrigido bug em que não estavam a ser lidos 16 bytes do ficheiro

--- src/file_operations.c
+++ src/file_operations.c
@@ -1,13 +1,14 @@
 #include <unistd.h>
 #include "file_operations.h"
+#include "block.h"
 
 int inputFd = -1;
 int outputFd = -1;
 
 int readBlock(int blockPosition, Block block) {
-  return (read(inputFd, block, sizeof(block)) > 0);
+  return (read(inputFd, block, BLOCKSIZE) > 0);
 }
 
 int writeBlock(int blockPosition, Block block) {
-  return (write(outputFd, block, sizeof(block)) > 0);
+  return (write(outputFd, block, BLOCKSIZE) > 0);
 }
